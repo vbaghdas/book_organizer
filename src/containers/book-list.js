d90e3e7811b27c76f8aaefe53194f5e1943ec774
vbaghdas <vbaghdas@gmail.com>
2017-11-13 00:11:34 -0800

    completed managing app state with redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li className="list-group-item" key={book.title}>{book.title}</li>
+                <li 
+                    className="list-group-item" 
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}>
+                    {book.title}
+                </li>
             );
         });
     }
@@ -25,4 +32,8 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch)  {
+    return bindActionCreators({ selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
